20b58ded2b5f8c039b5d640b54e93b140e4c0039
Zaldy C. Aquino Jr. <zanoakino.22@gmail.com>
2026-07-14 14:21:24 +0800

    Removed the password change requirement for new admins

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,6 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'role' => 'admin', // Forces role to admin instead of default 'clerk'
-            'must_change_password' => 1, // Ensures security compliance for new admin
         ]);
 
         event(new Registered($user));
