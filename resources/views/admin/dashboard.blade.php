ed6d97ad3416ac2535e932697b0b2b1887f49277
Zaldy C. Aquino Jr. <zanoakino.22@gmail.com>
2026-07-18 03:48:23 +0800

    removed a line of code on the dashboard of the admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <h1 class="text-2xl font-bold text-gray-900">Welcome Administrator!</h1>
-                <p class="text-gray-600 mt-2">Use the navigation bar above to manage Chapter rows or provision administrative system accounts.</p>
             </div>
         </div>
     </div>
