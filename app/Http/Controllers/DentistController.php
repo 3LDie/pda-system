41afef5ce6a62a98b206cbe0b64a2c0adcd02987
Zaldy C. Aquino Jr. <zanoakino.22@gmail.com>
2026-07-18 15:06:17 +0800

    Add debug log for user ID

--- app/Http/Controllers/DentistController.php
+++ app/Http/Controllers/DentistController.php
@@ -20,6 +20,7 @@ class DentistController extends Controller
 
     public function index(Request $request)
     {
+        Log::info('Current User ID: ' . auth()->id());
         $query = User::where('users.role', 'member')
             ->join('dentist_profiles', 'users.id', '=', 'dentist_profiles.user_id')
             ->select('users.*', 'dentist_profiles.id as profile_id', 'dentist_profiles.full_name', 'dentist_profiles.prc_no', 'dentist_profiles.contact_no', 'dentist_profiles.clinic_address', 'dentist_profiles.profile_image');
