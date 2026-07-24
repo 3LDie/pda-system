af93a6017c99da8f55d6eb20724db627f70c9527
gailgail123 <anathaliegailcastillo11@gmail.com>
2026-07-24 16:16:39 +0800

    change features

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,18 +3,17 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
-    public function run($this->call(DentistTestSeeder::class);): void
+    public function run(): void
     {
+        $this->call(DentistTestSeeder::class);
+
         // User::factory(10)->create();
 
         User::factory()->create([
